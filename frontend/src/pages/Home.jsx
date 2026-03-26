0b1cbd4c792800a21c8d4fb683e8a582582a0efd
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-26 15:53:10 +0545

    Tailers section completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import Movies from '../components/Movies'
+import Trailers from '../components/Trailers'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar/>
         <Banner/>
         <Movies/>
+        <Trailers/>
     </div>
   )
 }
