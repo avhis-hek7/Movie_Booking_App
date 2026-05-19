9a65c4823fd3f65cd8fad01ac08affcb37088663
avhis-hek7 <sharmaavishek679@gmail.com>
2026-05-19 18:15:15 +0545

    release page completed as well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Movie from './pages/Movie'
+import Release from './pages/Release'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<SignUp/>} />
       <Route path='/movies' element={<Movie/>} />
+      <Route path='/releases' element={<Release/>}/>
    </Routes>
    </>
   )
---
+++ frontend/src/pages/Release.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import ReleasePage from '../components/ReleasePage'
+
+const Release = () => {
+  return (
+    <div>
+
+        <Navbar/>
+        <ReleasePage/>
+        <Footer/>
+
+
+
+    </div>
+  )
+}
+
+export default Release
